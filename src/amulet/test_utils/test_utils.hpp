38481db6d492088006abca61a77fcca86d615ddb
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-14 14:37:53 +0100

    Add comparison functions and fix some issues
    Add !=, <, <=, >, >= assertions.
    Support classes that are not default constructable.
    Fix variable name conflictions.
    Fix incorrect variable in comparison message.

--- src/amulet/test_utils/test_utils.hpp
+++ src/amulet/test_utils/test_utils.hpp
@@ -14,81 +14,92 @@ std::string cast_to_string(const T& obj)
     }
 }
 
-#define ASSERT_EQUAL(CLS, A, B)                        \
-    {                                                  \
-        CLS a;                                         \
-        try {                                          \
-            a = A;                                     \
-        } catch (const std::exception& e) {            \
-            std::string msg;                           \
-            msg.reserve(200);                          \
-            msg += "Failed evaluating A in file ";     \
-            msg += __FILE__;                           \
-            msg += " at line ";                        \
-            msg += std::to_string(__LINE__);           \
-            msg += ". ";                               \
-            msg += e.what();                           \
-            throw std::runtime_error(msg);             \
-        }                                              \
-        CLS b;                                         \
-        try {                                          \
-            b = B;                                     \
-        } catch (const std::exception& e) {            \
-            std::string msg;                           \
-            msg.reserve(200);                          \
-            msg += "Failed evaluating B in file ";     \
-            msg += __FILE__;                           \
-            msg += " at line ";                        \
-            msg += std::to_string(__LINE__);           \
-            msg += ". ";                               \
-            msg += e.what();                           \
-            throw std::runtime_error(msg);             \
-        }                                              \
-        if (a != b) {                                  \
-            std::string msg;                           \
-            msg.reserve(200);                          \
-            msg += "Values are not equal in file ";    \
-            msg += __FILE__;                           \
-            msg += " at line ";                        \
-            msg += std::to_string(__LINE__);           \
-            msg += ".";                                \
-            auto a_msg = cast_to_string(a);            \
-            if (!a_msg.empty()) {                      \
-                msg += " Expected \"" + a_msg + "\"."; \
-            }                                          \
-            auto b_msg = cast_to_string(b);            \
-            if (!a_msg.empty()) {                      \
-                msg += " Got \"" + b_msg + "\".";      \
-            }                                          \
-            throw std::runtime_error(msg);             \
-        }                                              \
+#define _ASSERT_COMPARE(CLS, A, B, OP)                              \
+    {                                                               \
+        CLS assert_comp_a = [&]() {                                 \
+            try {                                                   \
+                CLS assert_comp_value = A;                          \
+                return assert_comp_value;                           \
+            } catch (const std::exception& e) {                     \
+                std::string assert_comp_msg;                        \
+                assert_comp_msg.reserve(200);                       \
+                assert_comp_msg += "Failed evaluating A in file ";  \
+                assert_comp_msg += __FILE__;                        \
+                assert_comp_msg += " at line ";                     \
+                assert_comp_msg += std::to_string(__LINE__);        \
+                assert_comp_msg += ". ";                            \
+                assert_comp_msg += e.what();                        \
+                throw std::runtime_error(assert_comp_msg);          \
+            }                                                       \
+        }();                                                        \
+        CLS assert_comp_b = [&]() {                                 \
+            try {                                                   \
+                CLS assert_comp_value = B;                          \
+                return assert_comp_value;                           \
+            } catch (const std::exception& e) {                     \
+                std::string assert_comp_msg;                        \
+                assert_comp_msg.reserve(200);                       \
+                assert_comp_msg += "Failed evaluating B in file ";  \
+                assert_comp_msg += __FILE__;                        \
+                assert_comp_msg += " at line ";                     \
+                assert_comp_msg += std::to_string(__LINE__);        \
+                assert_comp_msg += ". ";                            \
+                assert_comp_msg += e.what();                        \
+                throw std::runtime_error(assert_comp_msg);          \
+            }                                                       \
+        }();                                                        \
+        if (!(assert_comp_a OP assert_comp_b)) {                    \
+            std::string assert_comp_msg;                            \
+            assert_comp_msg.reserve(200);                           \
+            assert_comp_msg += "A " #OP " B failed in file ";       \
+            assert_comp_msg += __FILE__;                            \
+            assert_comp_msg += " at line ";                         \
+            assert_comp_msg += std::to_string(__LINE__);            \
+            assert_comp_msg += ".";                                 \
+            auto assert_comp_a_msg = cast_to_string(assert_comp_a); \
+            if (!assert_comp_a_msg.empty()) {                       \
+                assert_comp_msg += " A: " + assert_comp_a_msg;      \
+            }                                                       \
+            auto assert_comp_b_msg = cast_to_string(assert_comp_b); \
+            if (!assert_comp_b_msg.empty()) {                       \
+                assert_comp_msg += " B: " + assert_comp_b_msg;      \
+            }                                                       \
+            throw std::runtime_error(assert_comp_msg);              \
+        }                                                           \
     }
 
-#define ASSERT_RAISES(EXC, A)                         \
-    {                                                 \
-        bool err_raised = false;                      \
-        try {                                         \
-            A;                                        \
-        } catch (const EXC&) {                        \
-            err_raised = true;                        \
-        } catch (...) {                               \
-            std::string msg;                          \
-            msg.reserve(200);                         \
-            msg += "Other exception raised in file "; \
-            msg += __FILE__;                          \
-            msg += " at line ";                       \
-            msg += std::to_string(__LINE__);          \
-            msg += ". ";                              \
-            throw std::runtime_error(msg);            \
-        }                                             \
-        if (!err_raised) {                            \
-            std::string msg;                          \
-            msg.reserve(200);                         \
-            msg += "Exception not raised in file ";   \
-            msg += __FILE__;                          \
-            msg += " at line ";                       \
-            msg += std::to_string(__LINE__);          \
-            msg += ". ";                              \
-            throw std::runtime_error(msg);            \
-        }                                             \
+#define ASSERT_EQUAL(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, ==)
+#define ASSERT_NOT_EQUAL(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, !=)
+#define ASSERT_LESS(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, <)
+#define ASSERT_LESS_EQUAL(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, <=)
+#define ASSERT_GREATER(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, >)
+#define ASSERT_GREATER_EQUAL(CLS, A, B) _ASSERT_COMPARE(CLS, A, B, >=)
+
+#define ASSERT_RAISES(EXC, A)                                      \
+    {                                                              \
+        bool err_raised = false;                                   \
+        try {                                                      \
+            A;                                                     \
+        } catch (const EXC&) {                                     \
+            err_raised = true;                                     \
+        } catch (...) {                                            \
+            std::string assert_raise_msg;                          \
+            assert_raise_msg.reserve(200);                         \
+            assert_raise_msg += "Other exception raised in file "; \
+            assert_raise_msg += __FILE__;                          \
+            assert_raise_msg += " at line ";                       \
+            assert_raise_msg += std::to_string(__LINE__);          \
+            assert_raise_msg += ". ";                              \
+            throw std::runtime_error(assert_raise_msg);            \
+        }                                                          \
+        if (!err_raised) {                                         \
+            std::string assert_raise_msg;                          \
+            assert_raise_msg.reserve(200);                         \
+            assert_raise_msg += "Exception not raised in file ";   \
+            assert_raise_msg += __FILE__;                          \
+            assert_raise_msg += " at line ";                       \
+            assert_raise_msg += std::to_string(__LINE__);          \
+            assert_raise_msg += ". ";                              \
+            throw std::runtime_error(assert_raise_msg);            \
+        }                                                          \
     }
