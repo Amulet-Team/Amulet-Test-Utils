0f139b0b4e53e9131ea6b09238ff56f88ce8c6d4
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-05-14 14:41:52 +0100

    Add comparison functions and fix some issues (#2)
    Add !=, <, <=, >, >= assertions.
    Support classes that are not default constructable.
    Fix variable name conflictions.
    Fix incorrect variable in comparison message.

--- tests/test_amulet_test_utils/_test_test_utils.py.cpp
+++ tests/test_amulet_test_utils/_test_test_utils.py.cpp
@@ -34,6 +34,31 @@ static void test_assert_equal_6()
     ASSERT_EQUAL(std::string, "hello world", "hello world!")
 }
 
+static void test_assert_not_equal(int a, int b)
+{
+    ASSERT_NOT_EQUAL(int, a, b);
+}
+
+static void test_assert_less(int a, int b)
+{
+    ASSERT_LESS(int, a, b);
+}
+
+static void test_assert_less_equal(int a, int b)
+{
+    ASSERT_LESS_EQUAL(int, a, b);
+}
+
+static void test_assert_greater(int a, int b)
+{
+    ASSERT_GREATER(int, a, b);
+}
+
+static void test_assert_greater_equal(int a, int b)
+{
+    ASSERT_GREATER_EQUAL(int, a, b);
+}
+
 static void test_assert_raises_1()
 {
     ASSERT_RAISES(std::runtime_error, throw std::runtime_error(""))
@@ -56,6 +81,11 @@ PYBIND11_MODULE(_test_test_utils, m)
     m.def("test_assert_equal_4", &test_assert_equal_4);
     m.def("test_assert_equal_5", &test_assert_equal_5);
     m.def("test_assert_equal_6", &test_assert_equal_6);
+    m.def("test_assert_not_equal", &test_assert_not_equal);
+    m.def("test_assert_less", &test_assert_less);
+    m.def("test_assert_less_equal", &test_assert_less_equal);
+    m.def("test_assert_greater", &test_assert_greater);
+    m.def("test_assert_greater_equal", &test_assert_greater_equal);
     m.def("test_assert_raises_1", &test_assert_raises_1);
     m.def("test_assert_raises_2", &test_assert_raises_2);
     m.def("test_assert_raises_3", &test_assert_raises_3);
--- tests/test_amulet_test_utils/test_test_utils.py
+++ tests/test_amulet_test_utils/test_test_utils.py
@@ -22,6 +22,41 @@ class TestUtilsTestCase(TestCase):
         with self.assertRaises(RuntimeError):
             test_assert_equal_6()
 
+    def test_assert_not_equal(self) -> None:
+        from test_amulet_test_utils._test_test_utils import test_assert_not_equal
+        test_assert_not_equal(5, 6)
+        test_assert_not_equal(6, 5)
+        with self.assertRaises(RuntimeError):
+            test_assert_not_equal(5, 5)
+        with self.assertRaises(RuntimeError):
+            test_assert_not_equal(6, 6)
+
+    def test_assert_less(self) -> None:
+        from test_amulet_test_utils._test_test_utils import test_assert_less
+        test_assert_less(5, 6)
+        with self.assertRaises(RuntimeError):
+            test_assert_less(5, 5)
+
+    def test_assert_less_equal(self) -> None:
+        from test_amulet_test_utils._test_test_utils import test_assert_less_equal
+        test_assert_less_equal(5, 6)
+        test_assert_less_equal(5, 5)
+        with self.assertRaises(RuntimeError):
+            test_assert_less_equal(6, 5)
+
+    def test_assert_greater(self) -> None:
+        from test_amulet_test_utils._test_test_utils import test_assert_greater
+        test_assert_greater(6, 5)
+        with self.assertRaises(RuntimeError):
+            test_assert_greater(5, 5)
+
+    def test_assert_greater_equal(self) -> None:
+        from test_amulet_test_utils._test_test_utils import test_assert_greater_equal
+        test_assert_greater_equal(6, 5)
+        test_assert_greater_equal(5, 5)
+        with self.assertRaises(RuntimeError):
+            test_assert_greater_equal(5, 6)
+
     def test_assert_raises(self) -> None:
         from test_amulet_test_utils._test_test_utils import (
             test_assert_raises_1,
