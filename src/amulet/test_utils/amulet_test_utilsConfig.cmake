550a4c57b4e643a82877ed1a2a119adfd66ffb3c
gentlegiantJGC <gentlegiantJGC@users.noreply.github.com>
2025-06-11 10:05:02 +0100

    Fix cmake configuration

--- src/amulet/test_utils/amulet_test_utilsConfig.cmake
+++ src/amulet/test_utils/amulet_test_utilsConfig.cmake
@@ -1,8 +1,6 @@
 if (NOT TARGET amulet_test_utils)
     set(amulet_test_utils_INCLUDE_DIR "${CMAKE_CURRENT_LIST_DIR}/../..")
 
-    add_library(amulet_test_utils IMPORTED INTERFACE)
-    set_target_properties(amulet_test_utils PROPERTIES
-        INTERFACE_INCLUDE_DIRECTORIES "${amulet_test_utils_INCLUDE_DIR}"
-    )
+    add_library(amulet_test_utils INTERFACE)
+    target_include_directories(amulet_test_utils INTERFACE ${amulet_test_utils_INCLUDE_DIR})
 endif()
